4d88b31d54a48192b547614504457709624af693
angelwing888 <kyliecchang@gmail.com>
2025-10-12 23:29:41 -0700

    final

--- Interaction Model/Assets/Scripts/InteractionDetector.cs
+++ Interaction Model/Assets/Scripts/InteractionDetector.cs
@@ -17,6 +17,10 @@ public class InteractionDetector : MonoBehaviour
         if (context.performed)
         {
             interactableInRange?.Interact();
+            if (!interactableInRange.CanInteract())
+            {
+                interactionIcon.SetActive(false);
+            }
         }
     }
 
--- Interaction Model/Assets/Scripts/PlayerController.cs
+++ Interaction Model/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
