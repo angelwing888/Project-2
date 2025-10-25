e385d6d411d94233efbc49759ac4026173f08a1c
angelwing888 <kyliecchang@gmail.com>
2025-10-25 16:16:21 -0700

    menu done

---
+++ Interaction Model/Assets/Scripts/TabController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TabController : MonoBehaviour
+{
+    public Image[] tabImages;
+    public GameObject[] pages;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ActivateTab(0);
+    }
+
+    public void ActivateTab(int tabNum)
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+            tabImages[i].color = Color.grey;
+        }
+        pages[tabNum].SetActive(true);
+        tabImages[tabNum].color = Color.white;
+    }
+}
