5325eb32dc4c31c4d92e8167a41fd2d67adb3134
angelwing888 <kyliecchang@gmail.com>
2025-10-25 17:51:33 -0700

    save finished

--- Interaction Model/Assets/Scripts/SaveController.cs
+++ Interaction Model/Assets/Scripts/SaveController.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 public class SaveController : MonoBehaviour
 {
+    private string saveLocation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // define save location
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
+        Debug.Log("Save file path: " + saveLocation);
+
+
+        LoadGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveGame()
     {
+        SaveData saveData = new SaveData
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position,
+        };
         
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData.playerPosition;
+        }
+        else
+        {
+            SaveGame();
+        }
     }
 }
--- Interaction Model/Assets/Scripts/SaveData.cs
+++ Interaction Model/Assets/Scripts/SaveData.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
 
 public class SaveData
 {
-    
+    public Vector3 playerPosition;
 }
